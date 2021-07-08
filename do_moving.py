0c28d15583987c96538ac07322591c28508c07d6
akimasa-l <liumingzheng8@outlook.com>
2021-07-08 22:25:28 +0900

    動かすぞ～～～

--- do_moving.py
+++ do_moving.py
@@ -5,4 +5,7 @@ for i in ["png","py","json"]:
     a+=glob.glob(f"./*.{i}")
 print(a)
 for i in a:
+    if "do_moving" in i:
+        continue
     print(f"git mv {i} learned/")
+    subprocess.run(f"git mv {i} learned/")
