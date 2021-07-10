15b07505f6ecede692dc3ddd4e30e09b969709a6
akimasa-l <liumingzheng8@outlook.com>
2021-07-10 19:44:25 +0900

    画像大量生成できた！！！！！！！！！！！！！！！！！！！！！！！！！！

--- hobby/main.py
+++ hobby/main.py
@@ -10,18 +10,21 @@ print(datetime.datetime.now())
 # print(df["date"])
 # print(pandas.to_datetime(df["date"]).map(type))
 df["date"] = pandas.to_datetime(df["date"])
-def main(labels=["total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "population"],directory="images"):
+
+
+def main(labels=["total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "population"], directory="images"):
     for country in df["location"].unique():
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
         for vaccination in labels:
-    
+
             # vaccination="total_vaccinations"
             vaccine = df.dropna(subset=[vaccination])
             # print(vaccine["location"].value_counts())
             # print(vaccine["location"].unique())
             # country="United States"
-            america = vaccine.query(f"location==\"{country}\"", engine="numexpr")
+            america = vaccine.query(
+                f"location==\"{country}\"", engine="numexpr")
             if len(america) == 0:
                 continue
             # print(america)
@@ -29,7 +32,7 @@ def main(labels=["total_vaccinations", "people_vaccinated", "people_fully_vaccin
             america_vaccinations = america.set_index("date")[vaccination]
             # print(america_vaccinations)
             # print(type(america_vaccinations))
-    
+
             # print(america["date"].iat[0])
             america_vaccinations.plot(ax=ax)
             ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y %m/%d"))
@@ -41,8 +44,13 @@ def main(labels=["total_vaccinations", "people_vaccinated", "people_fully_vaccin
             ax.legend()
             ax.grid()
         plt.title(f"Vaccination in {country}")
-        plt.savefig("./{}/{}.png".format(directory,country.replace(" ", "_")))
+        plt.savefig("./{}/{}.png".format(directory, country.replace(" ", "_")))
         # plt.show()
         plt.close()
-main(labels=["total_vaccinations", "people_vaccinated", "people_fully_vaccinated"],directory="images/include_population")
+
+
+main(labels=["total_vaccinations", "people_vaccinated",
+             "people_fully_vaccinated"], directory="images/exclude_population")
+main(labels=["total_vaccinations", "people_vaccinated",
+             "people_fully_vaccinated", "population"], directory="images/include_population")
 print(datetime.datetime.now())
