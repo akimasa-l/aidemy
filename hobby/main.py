aee7ac29dbe7bbeb1b99a439e377cbc9ef98a570
akimasa-l <liumingzheng8@outlook.com>
2021-07-10 19:26:21 +0900

    画像生成しまくった！！！！！！！！！！！！！！

--- hobby/main.py
+++ hobby/main.py
@@ -4,34 +4,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.ticker import AutoLocator
-df=pandas.read_csv("./data.csv")
+df = pandas.read_csv("./data.csv")
 print(df)
 # print(df["date"])
 # print(pandas.to_datetime(df["date"]).map(type))
-df["date"]=pandas.to_datetime(df["date"])
-vaccination="total_vaccinations"
-vaccine=df.dropna(subset=[vaccination])
-print(vaccine["location"].value_counts())
-print(vaccine["location"].unique())
-country="United States"
-america=vaccine.query(f"location==\"{country}\"",engine="numexpr")
-print(america)
-print(type(america))
-america_vaccinations=america.set_index("date")[vaccination]
-print(america_vaccinations)
-print(type(america_vaccinations))
+df["date"] = pandas.to_datetime(df["date"])
+for country in df["location"].unique():
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for vaccination in ["total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]:
 
-fig= plt.figure()
-ax=fig.add_subplot(1,1,1)
-# print(america["date"].iat[0])
-america_vaccinations.plot(ax=ax)
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y %m/%d"))
-# ax.xaxis.set_major_locator( AutoLocator())
-ax.xaxis.set_major_locator( mdates.MonthLocator())
-# ax.yaxis.set_major_locator(AutoLocator())
-ax.set_xlim(america["date"].iat[0],america["date"].iat[-1])
-ax.legend()
-ax.grid()
-plt.title(f"Vaccination in {country}")
-plt.savefig("./{}.png".format(country.replace(" ","_")))
-plt.show()
+        # vaccination="total_vaccinations"
+        vaccine = df.dropna(subset=[vaccination])
+        print(vaccine["location"].value_counts())
+        print(vaccine["location"].unique())
+        # country="United States"
+        america = vaccine.query(f"location==\"{country}\"", engine="numexpr")
+        if len(america)==0:
+            continue
+        print(america)
+        print(type(america))
+        america_vaccinations = america.set_index("date")[vaccination]
+        print(america_vaccinations)
+        print(type(america_vaccinations))
+
+        # print(america["date"].iat[0])
+        america_vaccinations.plot(ax=ax)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y %m/%d"))
+        ax.xaxis.set_major_locator( AutoLocator())
+        # ax.xaxis.set_major_locator(mdates.MonthLocator())
+        # ax.yaxis.set_major_locator(AutoLocator())
+        ax.set_xlim(america["date"].iat[0], america["date"].iat[-1])
+        ax.legend()
+        ax.grid()
+    plt.title(f"Vaccination in {country}")
+    plt.savefig("./images/{}.png".format(country.replace(" ", "_")))
+    # plt.show()
+    plt.close()
