d57b6a767f1b4d9d6dece7a6f0d66f57649c89f7
akimasa-l <liumingzheng8@outlook.com>
2021-07-10 18:55:31 +0900

    グラフの概形ができた！！！！！！！！！！！

--- hobby/main.py
+++ hobby/main.py
@@ -1,13 +1,37 @@
 import pandas
 import datetime
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import AutoLocator
 df=pandas.read_csv("./data.csv")
 print(df)
 # print(df["date"])
 # print(pandas.to_datetime(df["date"]).map(type))
 df["date"]=pandas.to_datetime(df["date"])
-vaccine=df.dropna(subset=["total_vaccinations"])
+vaccination="total_vaccinations"
+vaccine=df.dropna(subset=[vaccination])
 print(vaccine["location"].value_counts())
+print(vaccine["location"].unique())
+country="United States"
+america=vaccine.query(f"location==\"{country}\"",engine="numexpr")
+print(america)
+print(type(america))
+america_vaccinations=america.set_index("date")[vaccination]
+print(america_vaccinations)
+print(type(america_vaccinations))
 
-
-
+fig= plt.figure()
+ax=fig.add_subplot(1,1,1)
+# print(america["date"].iat[0])
+america_vaccinations.plot(ax=ax)
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y %m/%d"))
+# ax.xaxis.set_major_locator( AutoLocator())
+ax.xaxis.set_major_locator( mdates.MonthLocator())
+# ax.yaxis.set_major_locator(AutoLocator())
+ax.set_xlim(america["date"].iat[0],america["date"].iat[-1])
+ax.legend()
+ax.grid()
+plt.title(f"Vaccination in {country}")
+plt.savefig("./{}.png".format(country.replace(" ","_")))
+plt.show()
